0f586826b662914d4af44d8d810cf5998f15764b
hnleee <lhn1455@gmail.com>
2022-09-05 17:13:22 +0900

    [add] functions : addProductToStore, getProduct

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -21,11 +21,47 @@ contract EcommerceStore {
         uint256 price;
         ProductCondition condition;
         address buyer;
-    }
+    } 
 
     constructor() {
         productIndex = 0;
     }
-3   
+
+    function addProductToStore(
+        uint256 _id, 
+        string memory _name, 
+        string memory _category, 
+        string memory _imageLink, 
+        string memory _descLink, 
+        uint256 _startTime, 
+        uint256 _price, 
+        ProductCondition _productCondition, 
+        address _buyer) public {
+        
+        productIndex += 1;
+        Product memory product = Product(productIndex, _name, _category, _imageLink,_descLink, _startTime,
+                                        _price, ProductCondition(_productCondition), address(0));
+        stores[msg.sender][productIndex] = product;
+        productIdInStore[productIndex] = payable(msg.sender);
+
+    }
+
+    function getProduct(uint256 _productId) public view returns(
+        uint256, 
+        string memory, 
+        string memory, 
+        string memory, 
+        string memory, 
+        uint256, 
+        uint256, 
+        ProductCondition, 
+        address) {
+        Product memory product = stores[productIdInStore[_productId]][_productId];
+        
+        return (product.id, product.name, product.category, product.imageLink, product.descLink,
+                product.startTime, product.price, product.condition, product.buyer);
+    }
+
+    
 
 }
